c6b386997472439c65f255bd307e3a17c9f209b1
tamami <tamami.oka@gmail.com>
2017-11-10 20:51:52 +0700

    tambahin hasil apk

--- app/src/main/java/lab/aikibo/e_desa/MainActivity.java
+++ app/src/main/java/lab/aikibo/e_desa/MainActivity.java
@@ -1,16 +1,23 @@
 package lab.aikibo.e_desa;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+        //        WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         WebView wv = (WebView) findViewById(R.id.webView);
