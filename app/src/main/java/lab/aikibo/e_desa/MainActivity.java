38f61749bfd9a97f945c36252e8c7341bcd90d9e
tamami <tamami.oka@gmail.com>
2017-11-10 21:46:50 +0700

    hide status bar

--- app/src/main/java/lab/aikibo/e_desa/MainActivity.java
+++ app/src/main/java/lab/aikibo/e_desa/MainActivity.java
@@ -16,8 +16,8 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
 
-        //this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-        //        WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         WebView wv = (WebView) findViewById(R.id.webView);
